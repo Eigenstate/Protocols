527065b2307560d7e6937f908f23b8b7e2041b22
Robin Betz <robin@robinbetz.com>
2018-02-07 15:19:17 -0800

    Remove Sherlock 1 handling

--- setup_amber.sh
+++ setup_amber.sh
@@ -1,60 +1,43 @@
 #!/bin/bash
 
-# Use these intel and cuda
-if [[ $SHERLOCK == 1 ]]; then
+# Sherlock 2, GCC build of Amber17 + patches
+#  Created at Mon Dec 11 17:01:06 PST 2017 via ./configure -mpi -noX11 --skip-python -nofftw3 -mpi -cuda gnu
+# Built 11 Dec 2017, RMB
+# No effort made for Sherlock 1
 
-    # Sometimes PI home is unset. Why? I dont know AUGH
-    export PI_HOME="/share/PI/rondror"
+if [[ $SHERLOCK == 2 ]]; then
+    module unload icc ifort imkl
 
-    # Okay sometimes module command is not defined here. That's dumb
-    # Let's setup the module environment manually
-    export MODULESHOME=/share/sw/free/lmod/lmod
-    export BASH_ENV=$MODULESHOME/init/bash
-    export MANPATH=$($MODULESHOME/libexec/addto MANPATH $MODULESHOME/share/man)
-    source $BASH_ENV >/dev/null                 # Module Support
-    export LMOD_PACKAGE_PATH=/share/sw/modules/ # where SitePackage.lua resides
-    export MODULEPATH_ROOT=/share/sw/modules
-    export MODULEPATH=/share/sw/modules/Core
+    module load cuda/8.0.61
+    module load devel
 
-    module load intel/2015
-    module load cuda/8.0
-
-# For sherlock 2, use the new installation
-elif [[ $SHERLOCK == 2 ]]; then
-    export AMBERHOME="$PI_HOME/software/amber16_gnu"
-    . $AMBERHOME/setup_amber.sh
-    return
+    # gcc + openmpi
+    module load openmpi
+    export MPI_HOME="/share/software/user/open/openmpi/2.1.1/"
+    export PATH="$MPI_HOME/bin:$PATH"                        # get mpicc etc
+    export CPATH="$MPI_HOME/include:$CPATH"                  # language independent include path
+    export LD_LIBRARY_PATH="$MPI_HOME/lib64:$LD_LIBRARY_PATH"  # search path at runtime
+    export LIBRARY_PATH="$MPI_HOME/lib64:$LIBRARY_PATH"     # search path at compile time (ld invocation)
 else
-    echo "SHERLOCK is unset! Doing nothing"
-    return
+    echo "Sherlock \"$SHERLOCK\" is unsupported by this amber"
+    exit 1
 fi
 
-# Amber wants MKL_HOME defined but module sets MKLROOT
-export MKL_HOME=$MKLROOT
-
-# Use our own intel compiled mpich 3.2
-export MPI_HOME="$PI_HOME/software/mpich-3.2"            # easier to type, some programs want this set
-export MPICC="$MPI_HOME/bin/mpicc"
-export MPICXX="$MPI_HOME/bin/mpicxx"
-export MPIF77="$MPI_HOME/bin/mpif77"
-export MPIF90="$MPI_HOME/bin/mpif90"
-
-export PATH="$MPI_HOME/bin:$PATH"                        # get mpicc etc
-export CPATH="$MPI_HOME/include:$CPATH"                  # language independent include path
-export LD_LIBRARY_PATH="$MPI_HOME/lib:$LD_LIBRARY_PATH"  # search path at runtime
-export LIBRARY_PATH="$MPI_HOME/lib:$LIBRARY_PATH"        # search path at compile time (ld invocation)
-
-# Make -lcuda work as libcuda is in stubs
+# Make -lcuda work
 export LD_LIBRARY_PATH="$CUDA_HOME/lib64/stubs:$LD_LIBRARY_PATH"
 export LIBRARY_PATH="$CUDA_HOME/lib64/stubs:$LIBRARY_PATH"
 
 # Set some envs
-export AMBERHOME="$PI_HOME/software/amber_dev"
+export AMBERHOME="$PI_HOME/software/amber16_gnu"
 export PATH="$AMBERHOME/bin:$PATH"
 export LD_LIBRARY_PATH="$AMBERHOME/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$AMBERHOME/lib64:$LD_LIBRARY_PATH"
 
 # stupid readline stuff
 export CPATH="/usr/include/readline/:$CPATH"
 export LD_LIBRARY_PATH="/usr/lib64:$LD_LIBRARY_PATH"
 
-
+# Debugging options
+#export FOR_DUMP_CORE_FILE=1
+#export FORT_BUFFERED=0
+#ulimit -c unlimited
